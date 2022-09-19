36d5cdbdee5a19d246df8c8907301ceaf483257d
yuzili <cdnrkj_yzl@163.com>
2022-09-19 21:47:55 +0800

    chore: create orthographic camera demo;

---
+++ src/pages/orthographic.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { createOrthographicCamera } from '~/utils/camera.js'
+const canvas = $ref()
+const viewLeft = $ref()
+const viewRight = $ref()
+onMounted(() => {
+  createOrthographicCamera(canvas, viewLeft, viewRight)
+})
+</script>
+
+<template>
+  <canvas ref="canvas" w-full h-full />
+  <div class="split-screen" w-full h-full flex position-absolute top-0 left-0>
+    <section id="view-left" ref="viewLeft" w-full h-full />
+    <section id="view-right" ref="viewRight" w-full h-full />
+  </div>
+</template>
+
